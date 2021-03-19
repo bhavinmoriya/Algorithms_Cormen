ef3c5968c5048b35293486b5ca90e273a0b09303
Bhavin <bhavinmoriya@gmail.com>
2021-03-18 21:16:52 -0300

    No need of l.sort()

--- DailyCoding.py
+++ DailyCoding.py
@@ -24,7 +24,7 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?'''
 
 def twoSum(l,k):
-  l.sort()
+  #l.sort()
   for i in range(len(l)):
     j = 0
     while j != i and j < len(l):
